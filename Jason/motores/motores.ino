4d1efa6d2d901a165cd0fb1b6a8643cdf2853915
veroarriola <v.arriola@ciencias.unam.mx>
2025-09-17 16:24:59 -0600

    Indentación

--- Jason/motores/motores.ino
+++ Jason/motores/motores.ino
@@ -1,20 +1,21 @@
 /**
-Práctica 1
-Autores:
-  César Arzate
-  Verónica E. Arriola
-(versión: Mega 2560)
-ARDUINO L293D(Puente H)
- 5  10
- 6  15
- 9   7
-10   2
-5V   1, 9, 16
-GND  4, 5, 12, 13
-El motor 1 se conecta a los pines 3 y 6 del Puente H
-El motor 2 se conecta a los pines 11 y 14 del Puente H
-La fuente de alimentacion de los Motores se conecta a tierra y
-el positivo al pin 8 del puennte H.
+ * Práctica 1
+ * Autores:
+ * César Arzate
+ * Verónica E. Arriola
+ * (versión: Mega 2560)
+ * ARDUINO L293D(Puente H)
+ * Conexiones a pines:
+ *  5  10
+ *  6  15
+ *  9   7
+ * 10   2
+ * 5V   1, 9, 16
+ * GND  4, 5, 12, 13
+ * El motor 1 se conecta a los pines 3 y 6 del Puente H
+ * El motor 2 se conecta a los pines 11 y 14 del Puente H
+ * La fuente de alimentacion de los Motores se conecta a tierra y
+ * el positivo al pin 8 del puennte H.
 */
 
 // Pines
